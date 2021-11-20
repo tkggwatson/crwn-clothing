7dfa116b4e22fc5da0651fa55d38cc69932e99b9
Graham Watson <graham.watson@thirdkindgames.com>
2021-11-20 23:28:59 +0000

    Convert MenuItem to use Link instead of onClick

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,24 +1,21 @@
 import React from 'react';
 
-import { useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 import './menu-item.style.scss';
 
-const MenuItem = ({ title, imageUrl, size, linkUrl }) => {
-    let navigate = useNavigate();
-    return (
-        <div className={`${size} menu-item`} onClick={() => navigate(linkUrl)}>
-            <div className='background-image'
-                style={{
-                    backgroundImage: `url(${imageUrl})`
-                }}
-            />
-            <div className='content'>
-                <h1 className='title'>{title.toUpperCase()}</h1>
-                <span className='subtitle'>SHOP NOW</span>
-            </div>
+const MenuItem = ({ title, imageUrl, size, linkUrl }) => (
+    <Link className={`${size} menu-item`} to={linkUrl}>
+        <div className='background-image'
+            style={{
+                backgroundImage: `url(${imageUrl})`
+            }}
+        />
+        <div className='content'>
+            <h1 className='title'>{title.toUpperCase()}</h1>
+            <span className='subtitle'>SHOP NOW</span>
         </div>
-    );
-};
+    </Link>
+);
 
 export default MenuItem;
