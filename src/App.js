6e9b472a4e92edde8fdf4d424ec076218846d602
Graham Watson <graham.watson@thirdkindgames.com>
2021-11-21 23:23:29 +0000

    Added basic header component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 
+import Header from './components/header/header.component';
+
 import './App.css';
 
 function App() {
   return (
     <div>
+      <Header />
       <Outlet />
     </div>
   );
