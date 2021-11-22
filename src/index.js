d62fe02cb5e9bf9cc896cb5dd33bbf2287f95220
Graham Watson <graham.watson@thirdkindgames.com>
2021-11-22 22:41:26 +0000

    Added page for when url doesn't match a route

--- src/index.js
+++ src/index.js
@@ -5,33 +5,27 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './index.css';
 
 import App from './App';
-import HomePage from './pages/homepage/homepage.component'
+import HomePage from './pages/homepage/homepage.component';
+import PageNotFound from './pages/page-not-found/page-not-found.component';
 import ShopPage from './pages/shop/shop.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<App />}>
-          <Route index element={<HomePage />} />
-          <Route path='shop' element={<ShopPage />} />
-          <Route path='signin' element={<SignInAndSignUpPage />} />
-          <Route
-            path="*"
-            element={
-              <main style={{ padding: "1rem" }}>
-                <p>There's nothing here!</p>
-              </main>
-            }
-          />
-        </Route>
-      </Routes>
-    </BrowserRouter>
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <BrowserRouter>
+            <Routes>
+                <Route path='/' element={<App />}>
+                    <Route index element={<HomePage />} />
+                    <Route path='shop' element={<ShopPage />} />
+                    <Route path='signin' element={<SignInAndSignUpPage />} />
+                    <Route path='*' element={<PageNotFound />} />
+                </Route>
+            </Routes>
+        </BrowserRouter>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
---
+++ src/pages/page-not-found/page-not-found.component.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const PageNotFound = () => (
+    <main style={{ padding: '1rem' }}>
+        <p>There's nothing here!</p>
+    </main>
+);
+
+export default PageNotFound;
