558a4ac4ebfc70cbe727e77a88fdb3a934d7d951
Graham Watson <graham.watson@thirdkindgames.com>
2021-11-24 13:53:50 +0000

    Moved routes to App component and added sign-in redirect if user is already signed in

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
-import { Outlet } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { onSnapshot } from '@firebase/firestore';
 
 import { createUserProfileDocument, onUserAuthStateChanged } from './firebase/firebase.utils';
 import { setCurrentUser } from './redux/user/user.actions';
 
-import Header from './components/header/header.component';
+import Layout from './components/layout/layout.component';
+import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
+import PageNotFound from './pages/page-not-found/page-not-found.component';
 
 import './App.css';
 
@@ -23,10 +27,9 @@ class App extends React.Component {
 
                 onSnapshot(userRef, (snapshot) => {
                     setCurrentUser({
-                            id: snapshot.id,
-                            ...snapshot.data(),
-                        },
-                    );
+                        id: snapshot.id,
+                        ...snapshot.data(),
+                    });
                 });
             } else {
                 setCurrentUser(null);
@@ -42,15 +45,28 @@ class App extends React.Component {
     render() {
         return (
             <div>
-                <Header />
-                <Outlet />
+                <Routes>
+                    <Route path='/' element={<Layout />}>
+                        <Route index element={<HomePage />} />
+                        <Route path='shop' element={<ShopPage />} />
+                        <Route
+                            path='signin'
+                            element={this.props.currentUser ? <Navigate replace to='/' /> : <SignInAndSignUpPage />}
+                        />
+                        <Route path='*' element={<PageNotFound />} />
+                    </Route>
+                </Routes>
             </div>
         );
     }
 }
 
+const mapStateToProps = ({ user }) => ({
+    currentUser: user.currentUser,
+});
+
 const mapDispatchToProps = (dispatch) => ({
     setCurrentUser: (user) => dispatch(setCurrentUser(user)),
 });
 
-export default connect(null, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/index.js
+++ src/index.js
@@ -1,15 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 
 import './index.css';
 
 import App from './App';
-import HomePage from './pages/homepage/homepage.component';
-import PageNotFound from './pages/page-not-found/page-not-found.component';
-import ShopPage from './pages/shop/shop.component';
-import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 
 import reportWebVitals from './reportWebVitals';
 import store from './redux/store';
@@ -18,14 +14,7 @@ ReactDOM.render(
     <React.StrictMode>
         <Provider store={store}>
             <BrowserRouter>
-                <Routes>
-                    <Route path='/' element={<App />}>
-                        <Route index element={<HomePage />} />
-                        <Route path='shop' element={<ShopPage />} />
-                        <Route path='signin' element={<SignInAndSignUpPage />} />
-                        <Route path='*' element={<PageNotFound />} />
-                    </Route>
-                </Routes>
+                <App />
             </BrowserRouter>
         </Provider>
     </React.StrictMode>,
